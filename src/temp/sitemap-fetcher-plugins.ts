c49227c61d139c96616b6d63f886c99aeccd06d4
Yevhen Povzlo <59564875+sc-YevhenPovzlo@users.noreply.github.com>
2023-03-02 13:07:14 +0200

    Add files via upload

---
+++ src/temp/sitemap-fetcher-plugins.ts
@@ -0,0 +1,2 @@
+export { disconnectedSitemapServicePlugin } from 'src/lib/sitemap-fetcher/plugins/disconnected-sitemap-service';
+export { graphqlSitemapServicePlugin } from 'src/lib/sitemap-fetcher/plugins/graphql-sitemap-service';
